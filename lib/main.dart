584e543e5f7d0cbc7ccb3a7de7366eb15241b899
Durjoy <swe.eng.durjoy15@gmail.com>
2025-06-28 23:55:24 +0600

    Today I learn about text color and boldy row,column

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,214 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
+
+
+
+/*
+// First day code of flutter
 void main(){
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text("Flutter"),
-        centerTitle: true,
+        title: Text("FLUTTER"),
+        centerTitle: true ,
       ),
-      body: Center(child: Text("Hello World")),
-
+      body: Center(child: Text("Body middle")),
     ),
   ));
 }
+
+*/
+
+import 'package:flutter/material.dart';
+/*
+void main(){
+  runApp(MaterialApp(
+      title: "Calculator",
+      home: SafeArea(
+          child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.blue,
+                title: Text("Calculator",style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),),
+                centerTitle: true,
+              ),
+
+              body: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Text("Text -1",style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),),
+                    Text("Text -1",style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),),
+                    Text("Text -1",style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),),
+                    Text("Text -1",style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),),
+
+
+
+                  ],
+
+                ),
+              )
+          ))
+  ));
+}
+
+*/
+
+/*
+void main(){
+  runApp(Center(
+      child: Text(
+        "Hello the World",
+        style: TextStyle(
+          fontSize: 35,
+          color: Colors.deepPurpleAccent,
+          fontWeight: FontWeight.w400,
+          overflow: TextOverflow.ellipsis,
+        ),
+        textDirection: TextDirection.ltr,)));
+}
+
+
+*/
+
+/*
+void main(){
+  runApp(MaterialApp(
+    title: "Calculator",
+    home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text("Calculator",style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),),
+            centerTitle: true,
+          ),
+          
+          body: Center(child: Text("Hello world", style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.w800,
+            fontSize: 40,
+          ),)),
+        ))
+  ));
+}
+
+*/
+
+void main(){
+  runApp(MaterialApp(
+      title: "Calculator",
+      home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: Text("Calculator",style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),),
+              centerTitle: true,
+            ),
+
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+
+                  Text("Text -1",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -2",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -3",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -4",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -5",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -6",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -7",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -8",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -9",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -10",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -11",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -12",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -13",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -14",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -15",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+                  Text("Text -16",style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),),
+
+
+
+                ],
+
+              ),
+            )
+          ))
+  ));
+}
+
+
+
+
